6714cbdf7b5c9668d803965c62578a6b0cdb07e5
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-08-26 23:16:41 -0600

    backend model, model interface, and model database completed.

---
+++ src/main/java/com/fullStack/springReact/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.fullStack.springReact.repository;
+
+import com.fullStack.springReact.model.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+}
